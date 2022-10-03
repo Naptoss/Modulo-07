55900a9ddfc8981dc9c958bd860d3d2a187d3e95
antonio <tomgabinio@gmail.com>
2022-10-03 10:54:22 -0300

    .

--- SQLite/bd_sqlite.py
+++ SQLite/bd_sqlite.py
@@ -40,12 +40,13 @@ cursor = conexao.cursor()
 
 # conexao.commit()
 
-cursor.execute('SELECT * FROM clientes')
+cursor.execute('SELECT nome, peso FROM clientes WHERE peso> :peso',
+               {'peso': 50}) # ele elimina pessoas que tem menos de 50kg 
 
 for linha in cursor.fetchall():
-    identificador, nome, peso = linha
+    nome, peso = linha
 
-    print(identificador, nome, peso)
+    print(nome, peso)
 
 cursor.close()
 conexao.close()
